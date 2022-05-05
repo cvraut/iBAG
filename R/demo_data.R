25134ab6db2d408b1e44f790b034e238f1b115e4
Chinmay Raut <craut@cn011.bio.sph.umich.edu>
2022-05-05 16:56:44 -0400

    more rework of the iBAG_data obj

--- R/demo_data.R
+++ R/demo_data.R
@@ -15,8 +15,7 @@
 #' @name demo_meth
 #' @usage data(demo_meth)
 #' @format A data frame with 770 rows and 1218 variables
-#'
-#' @export
+#' @source https://portal.gdc.cancer.gov/
 demo_meth <-read.csv(system.file("data","meth.csv",package = "iBAG"),row.names = 1,header = TRUE)
 
 #' demo_mrna
@@ -30,8 +29,7 @@ demo_meth <-read.csv(system.file("data","meth.csv",package = "iBAG"),row.names =
 #' @name demo_mrna
 #' @usage data(demo_mrna)
 #' @format A data frame with 770 rows and 1218 variables
-#'
-#' @export
+#' @source https://portal.gdc.cancer.gov/
 demo_mrna <-read.csv(system.file("data","mrna.csv",package = "iBAG"),row.names = 1,header = TRUE)
 
 #' demo_cnv
@@ -46,8 +44,7 @@ demo_mrna <-read.csv(system.file("data","mrna.csv",package = "iBAG"),row.names =
 #' @name demo_cnv
 #' @usage data(demo_cnv)
 #' @format A data frame with 770 rows and 1218 variables
-#'
-#' @export
+#' @source https://portal.gdc.cancer.gov/
 demo_cnv <-read.csv(system.file("data","cnv.csv",package = "iBAG"),row.names = 1,header = TRUE)
 
 #' demo_outcome
@@ -59,6 +56,5 @@ demo_cnv <-read.csv(system.file("data","cnv.csv",package = "iBAG"),row.names = 1
 #' @name demo_outcome
 #' @usage data(demo_outcome)
 #' @format A data frame with 770 rows and 1 variables
-#'
-#' @export
+#' @source https://portal.gdc.cancer.gov/
 demo_outcome <-read.csv(system.file("data","outcome.csv",package = "iBAG"),row.names = 1,header = TRUE)
--- R/objects.R
+++ R/objects.R
@@ -11,11 +11,9 @@ library(R6)
 #' @description R6 class to store and manage the iBAG data.
 #' This does not contain or manage model result data.
 #'
-#' @export
 iBAG_data <- R6Class("iBAG_data",
   public = list(
-    #' initialize
-    #'
+    #' @description constructor
     #' @usage iBAG_data$new()
     #'
     #' @param mrna (iBAG::demo_mrna) dataframe of mrna data
@@ -25,10 +23,9 @@ iBAG_data <- R6Class("iBAG_data",
     #' @param validate (TRUE) to validate the data supplied. Will raise Error if data is not valid.
     #' @param one_val_per_gene (TRUE) whether all the upstream data has 1 column per gene or not.
     #' @param ... : ...
-    #'
     initialize = function(mrna = iBAG::demo_mrna,
                           outcome = iBAG::demo_outcome,
-                          data = list(cnv=iBAG::demo_cnv),
+                          data = list(cnv=iBAG::demo_cnv,meth=iBAG::demo_meth),
                           DEBUG = FALSE,
                           validate = TRUE,
                           one_val_per_gene = TRUE,...){
@@ -58,31 +55,27 @@ iBAG_data <- R6Class("iBAG_data",
       private$n_patients <- length(private$patients)
       private$n_data <- length(private$data_names)
     },
-    #' get.data
-    #'
     #' @description get the dataset specified by user
     #' @usage iBAG_data$new()$get.data()
     #'
-    #' @param index (1): index of the dataset to return. Can either be an integer otherwise it's treated as a string. Can be null (sends entire list back).
+    #' @param index (NULL): index of the dataset to return. Can either be an integer otherwise it's treated as a string. Can be null (sends entire list back).
     #' @return dataframe of the requested dataset or list of all dataframes
     #'
     #' TODO: address not found/out of bounds conditions
-    get.data = function(index = 1){
+    get.data = function(index = NULL){
       if(private$DEBUG){
         print(index)
         print(dim(private$data[[index]]))
       }
       if(is.null(index)){
         return(private$data)
-      } else if(is.numeric(index) & index == as.integer(index)){
+      } else if(is.numeric(index) && index == as.integer(index)){
         return(private$data[[index]])
       } else {
         index = paste(index)
         return(private$data[index][[1]])
       }
     },
-    #' get.mrna
-    #'
     #' @description get the mrna dataset
     #' @usage iBAG_data$new()$get.mrna()
     #'
@@ -105,8 +98,6 @@ iBAG_data <- R6Class("iBAG_data",
       }
       return(private$outcome)
     },
-    #' get.n_data
-    #'
     #' @description get the number of upstream datasets
     #' @usage iBAG_data$new()$get.n_data()
     #'
@@ -123,8 +114,6 @@ iBAG_data <- R6Class("iBAG_data",
     get.n_patients = function(){
       return(dim(private$mrna)[1])
     },
-    #' get.patients
-    #'
     #' @description get a vector of patients in dataset
     #' @usage iBAG_data$new()$get.patients()
     #'
@@ -141,8 +130,6 @@ iBAG_data <- R6Class("iBAG_data",
     get.n_genes = function(){
       return(dim(private$mrna)[2])
     },
-    #' get.genes
-    #'
     #' @description get a vector of genes in dataset
     #' @usage iBAG_data$new()$get.genes()
     #'
@@ -152,16 +139,28 @@ iBAG_data <- R6Class("iBAG_data",
     }
   ),
   private = list(
+    # roxygen was being pissy about documenting private variables, but I left em in ;)
+    # @field data list of dataframes for the upstream data
     data = NULL,
+    # @field mrna dataframe for the mrna data
     mrna = NULL,
+    # @field outcome dataframe for the outcome data
     outcome = NULL,
+    # @field DEBUG boolean flag used to figure out what went wrong or print stuff to console
     DEBUG = NULL,
+    # @field one_val_per_gene boolean flag used to define whether there's only 1 column of data per gene in upstream data or not (used to speed up computations).
     one_val_per_gene = NULL,
+    # @field genes vector of gene names (taken from colnames of mrna)
     genes = NULL,
+    # @field patients vector of patient names (taken from row.names of mrna)
     patients = NULL,
+    # @field data_names vector of data source names (taken from names(data))
     data_names = NULL,
+    # @field n_genes number of genes (length(genes))
     n_genes = NULL,
+    # @field n_patients number of patients (length(patients))
     n_patients = NULL,
+    # @field n_data number of data (length(data_names))
     n_data = NULL
   )
 )
