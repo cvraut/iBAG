6035a82e54ae761fa5646573d0b1da6d57db206e
Chinmay Raut <craut@cn022.bio.sph.umich.edu>
2022-05-12 20:49:57 -0400

    mechmodel works for demo_data, it is really slow tho...

--- R/mechmodel.R
+++ R/mechmodel.R
@@ -3,84 +3,10 @@
 
 library(mgcv)
 
-#' mechmodel
-#'
-#' @param meth
-#' @param data.list
-#' @param DEBUG
-#' @param ...
-#'
-#' @export
-mech.model.single_gene_val <- function(mrna, data.list, DEBUG=TRUE, ...){
-  genes <- colnames(mrna)
-  n <- nrow(mrna)
-  p <- length(genes)
-  k <- length(data.list) + 1
-
-  X <- matrix(NA, nrow=n, ncol=p*k)
-  row.names(X) <- rownames(mrna)
-  colnames(X) <- paste(rep(sapply(1:k,FUN = function(i){sprintf(ifelse(i<k,"data_%d","other"),i)}),each=p),rep(genes,k),sep="_")
-  SS <- matrix(NA,nrow=p,ncol = k+1)
-  colnames(SS) <- c("SST",sapply(1:(k-1),FUN = function(i){sprintf("SSD%d",i)}),"SSO")
-  row.names(SS) <- genes
-
-  # TODO: check if gene data has been collapsed & collapse it if needed
-  get_gene <- function(gene_i,data.list){
-    all_data <- sapply(1:(k-1), function(data_i){
-      ind_data <- grep(genes[gene_i],colnames(data.list[[data_i]]));
-      return(data.list[[data_i]][,ind_data])
-    })
-    return(all_data)
-  }
-
-
-  process_gene <- function(gene_i){
-    if(mean(mrna[,gene_i]) != 0){
-      mrna[,gene_i] <- mrna[,gene_i] - mean(mrna[,gene_i])
-      if(DEBUG){
-        cat("Someone did not zero-mean the mrna >:(\n")
-      }
-    }
-
-    scores_data <- get_gene(gene_i,data.list)
-    scores.form <- paste(sapply(1:(k-1),FUN=function(i){sprintf("s(scores_data[,%d])",i)}),collapse = ' + ')
-    mrna.1 <- mrna[,gene_i]
-    formula_all <- paste(sprintf("mrna.1 ~ -1 + "),scores.form)
-    if(DEBUG){
-      cat(sprintf("Gene: %d\nFormula: %s\n",gene_i,formula_all))
-    }
-    gam.mrna  <- gam(as.formula(formula_all))
-    fit_scores <- as.matrix(predict.gam(gam.mrna,type="terms"))
-    if(DEBUG){
-      cat("gam_fit done!")
-    }
-    sapply(1:(k-1),FUN = function(i){
-      X[,paste(sprintf("data_%d",i),genes[gene_i],sep="_")] <<- fit_scores[,i];
-      SS[gene_i,i+1] <<- sum( ( (fit_scores[,i]) - mean(mrna[,gene_i]) )^2  )
-    })
-
-    O <- gam.mrna$residuals
-    X[,paste("other",genes[gene_i],sep="_")] <<- O
-    # Pseudo Sums of Squares (to use to find percentages of explained variance)
-    SS[gene_i,1] <<- sum( (mrna[,gene_i] - mean(mrna[,gene_i]))^2 )
-    if(k>2){
-      SS[gene_i,(k+1)] <<- SS[gene_i,1] - sum(SS[gene_i,2:k])
-    } else {
-      SS[gene_i,3] <<- SS[gene_i,1] - SS[gene_i,2]
-    }
-    if(DEBUG){
-      print(dim(SS))
-    }
-  }
-  # note parallel later ...
-  sapply(1:p,FUN=function(i){process_gene(i)})
-  return(list(X=X,
-              SS=SS))
-}
-
 #' empty.X.constructor
 #'
 #' @description This is a helper function to create & name the rows & columns of X appropriately
+#' @param k this is the number of datasets (not including the other/residual, gets added automatically)
 empty.X.constructor <- function(patients,
                                 genes,
                                 k,
@@ -90,7 +16,7 @@ empty.X.constructor <- function(patients,
                                 DEBUG = FALSE){
   n = length(patients)
   p = length(genes)
-  X <- matrix(NA, nrow=n, ncol=p*k)
+  X <- matrix(NA, nrow=n, ncol=p*(k+1))
   row.names(X) <- patients
 
   if(other.name %in% data.names){
@@ -98,13 +24,23 @@ empty.X.constructor <- function(patients,
   }
 
   X.namer <- function(i){
-    return(sprintf(ifelse(i<k,data.names[i],other.name),i))
+    return(sprintf(ifelse(i<=k,data.names[i],other.name),i))
   }
-  colnames(X) <- paste(rep(sapply(1:k,FUN = X.namer),each=p),rep(genes,k),sep="_")
+  colnames(X) <- paste(rep(sapply(1:(k+1),FUN = X.namer),each=p),rep(genes,k),sep="_")
   return(X)
 }
 
 #' mech.model
+#'
+#' @description This fits the mechanistic model for iBAG
+#'
+#' @param mrna the mrna data. Look at iBAG::demo_mrna for example format
+#' @param data.list a list of the upstream data
+#' @param sep (_): the character value to use as a seperator
+#' @param other.name ("other"): the string to use to label residuals from mechmodel
+#' @param default.data.name ("data"): the string to use to label unnamed datasets from data.list
+#' @param DEBUG (FALSE): debug flag
+#' @param ...
 #' @export
 mech.model <- function(mrna,
                        data.list,
@@ -116,16 +52,28 @@ mech.model <- function(mrna,
   n <- nrow(mrna)
   p <- length(genes)
   k <- length(data.list) + 1
-
-  X <- empty.X.constructor(row.names(mrna), genes, k, names(data.list), other.name, sep, default.data.name, DEBUG)
+  data_names <- get.data.names(data.size = k-1,
+                               data.names = names(data.list),
+                               sep = sep,
+                               default.data.name = default.data.name,
+                               DEBUG = DEBUG)
+  X <- empty.X.constructor(patients = row.names(mrna),
+                           genes = genes,
+                           k = (k-1),
+                           data.names = data_names,
+                           other.name = other.name,
+                           sep = sep,
+                           DEBUG = DEBUG)
   SS <- matrix(NA,nrow=p,ncol = k+1)
-  colnames(SS) <- c("SST",sapply(1:(k-1),FUN = function(i){sprintf("SSD%d",i)}),"SSO")
+  colnames(SS) <- c("SST",sapply(1:(k-1),FUN = function(i){sprintf("SS%s%s",sep,data_names[i])}),"SSO")
   row.names(SS) <- genes
 
   # TODO: check if gene data has been collapsed & collapse it if needed
+  # TODO: support multiple prob information
   get_gene <- function(gene_i,data.list){
     all_data <- sapply(1:(k-1), function(data_i){
-      ind_data <- grep(genes[gene_i],colnames(data.list[[data_i]]));
+      gene_regex <- paste("^",sprintf("%s(%s.+)?",genes[gene_i],sep),"$",sep = "")
+      ind_data <- grep(gene_regex,colnames(data.list[[data_i]]));
       return(data.list[[data_i]][,ind_data])
     })
     return(all_data)
@@ -153,12 +101,12 @@ mech.model <- function(mrna,
       cat("gam_fit done!")
     }
     sapply(1:(k-1),FUN = function(i){
-      X[,paste(sprintf("data_%d",i),genes[gene_i],sep="_")] <<- fit_scores[,i];
+      X[,gene_i+(i-1)*p] <<- fit_scores[,i];
       SS[gene_i,i+1] <<- sum( ( (fit_scores[,i]) - mean(mrna[,gene_i]) )^2  )
     })
 
     O <- gam.mrna$residuals
-    X[,paste("other",genes[gene_i],sep="_")] <<- O
+    X[,gene_i+(k-1)*p] <<- O
     # Pseudo Sums of Squares (to use to find percentages of explained variance)
     SS[gene_i,1] <<- sum( (mrna[,gene_i] - mean(mrna[,gene_i]))^2 )
     if(k>2){
@@ -170,7 +118,7 @@ mech.model <- function(mrna,
       print(dim(SS))
     }
   }
-  # note parallel later ...
+  # TODO: parallel later ...
   sapply(1:p,FUN=function(i){process_gene(i)})
   return(list(X=X,
               SS=SS))
--- tests/testthat/test_mechmodel.R
+++ tests/testthat/test_mechmodel.R
@@ -2,13 +2,18 @@ library(iBAG)
 
 testthat::test_that("test single gene value mech model", {
   demo_data <- iBAG::iBAG_data$new()
-  testthat::expect_silent(res <- iBAG::mech.model.single_gene_val(demo_data$get.mrna(),demo_data$get.data()))
+  testthat::expect_silent(res <- iBAG::mech.model(demo_data$get.mrna(),demo_data$get.data()))
 })
 
 testthat::test_that("test empty.X.constructor", {
   demo_data <- iBAG::iBAG_data$new()
-  patients = demo_data$get.patients(),
-  genes = demo_data$get.genes(),
-  k = demo_data$get.n_data(),
-  data.names = demo_data$get.data_names()
+  actual.X <- empty.X.constructor(patients = demo_data$get.patients(),
+                                  genes = demo_data$get.genes(),
+                                  k = demo_data$get.n_data(),
+                                  data.names = demo_data$get.data_names())
+  expected.X <- matrix(NA,nrow = 770,ncol = 3*1218)
+  row.names(expected.X) <- demo_data$get.patients()
+  genes <- demo_data$get.genes()
+  colnames(expected.X) <- as.vector(unname(sapply(c("cnv","meth","other"), function(platform){paste(platform,genes,sep = "_")})))
+  testthat::expect_identical(actual.X,expected.X,label = "test that empty.X works with demo data")
 })
